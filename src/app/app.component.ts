14bf72ddd1315ae8af682382e730bb5ca2aef081
rodolf <rodolflenoir@hotmail.com>
2018-04-13 17:00:02 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from "firebase";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(){
+    var config = {
+      apiKey: "AIzaSyCYZuXJprXWXOXh9XpvZ0o8K3XTLOnN44U",
+      authDomain: "bookstore-13db0.firebaseapp.com",
+      databaseURL: "https://bookstore-13db0.firebaseio.com",
+      projectId: "bookstore-13db0",
+      storageBucket: "bookstore-13db0.appspot.com",
+      messagingSenderId: "946882021273"
+    };
+    firebase.initializeApp(config);
+  }
 }
